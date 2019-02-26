4661f4b3c005f6ad5b1418fda25352869e315d1a
Patrick Kim <pkim050@gmail.com>
2019-02-26 06:30:13 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -3,14 +3,20 @@ class Owner
   attr_accessor :mood
   attr_reader :name
   @@all = []
+  @@count = 0
   
   def initialize(name)
     @name = name
     @mood = "nervous"
+    @@count += 1
     @@all << self
   end
   
   def self.all
     @@all
+  end
+  
+  def self.count
+    @@count
   end
 end
